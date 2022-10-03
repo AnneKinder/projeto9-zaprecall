da17812e2b0bd6b9f5cb02b0eca21e6e5309afda
Anne Kindermann <102705688+AnneKinder@users.noreply.github.com>
2022-10-03 15:07:23 -0300

    GoTzando

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,45 +1,45 @@
 let cards = [
-    {
-      numberQ: "Pergunta 1",
-      question: "O que é JSX?",
-      answer: "Uma extensão de linguagem do JavaScript"
-    },
-    {
-      numberQ: "Pergunta 2",
-      question: "O React é __",
-      answer: "uma biblioteca JavaScript para construção de interfaces"
-    },
-    {
-      numberQ: "Pergunta 3",
-      question: "Componentes devem iniciar com __",
-      answer: "letra maiúscula"
-    },
-    {
-      numberQ: "Pergunta 4",
-      question: "Podemos colocar __ dentro do JSX",
-      answer: "expressões"
-    },
-    {
-      numberQ: "Pergunta 5",
-      question: "O ReactDOM nos ajuda __",
-      answer: "interagindo com a DOM para colocar componentes React na mesma"
-    },
-    {
-      numberQ: "Pergunta 6",
-      question: "Usamos o npm para __",
-      answer: "gerenciar os pacotes necessários e suas dependências"
-    },
-    {
-      numberQ: "Pergunta 7",
-      question: "Usamos props para __",
-      answer: "passar diferentes informações para componentes "
-    },
-    {
-      numberQ: "Pergunta 8",
-      question: "Usamos estado (state) para __ ",
-      answer:
-        "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
-    }
-  ];
+  {
+    numberQ: "Pergunta 1",
+    question: "Qual é o título completo de Daenerys Targaryen?",
+    answer:
+      "Daenerys Targaryen, Filha da Tormenta, a Não Queimada, Mãe de Dragões, Rainha de Mereen, Rainha dos Ândalos e dos Primeiros Homens, Quebradora de Correntes, Senhora dos Sete Reinos, Khaleesi dos Dothraki, a Primeira de Seu Nome, "
+  },
+  {
+    numberQ: "Pergunta 2",
+    question: "Qual é o verdadeiro nome de Jon Snow?",
+    answer: "Aegon Targaryen."
+  },
+  {
+    numberQ: "Pergunta 3",
+    question: "Quantos anos tinha a Mulher Vermelha?",
+    answer: "Aproximadamente 400 anos."
+  },
+  {
+    numberQ: "Pergunta 4",
+    question: "The night is dark and...",
+    answer: "... full of terrors."
+  },
+  {
+    numberQ: "Pergunta 5",
+    question: "Valar morghulis.",
+    answer: "Valar dohaeris."
+  },
+  {
+    numberQ: "Pergunta 6",
+    question: "Quem envenenou o Rei Joffrey?",
+    answer: "Lady Olena."
+  },
+  {
+    numberQ: "Pergunta 7",
+    question: "Em que episódio Daenerys sentou no Trono de Ferro?",
+    answer: "Ela não sentou."
+  },
+  {
+    numberQ: "Pergunta 8",
+    question: "Os 3 dragões de Daenerys:",
+    answer: "Drogon, Viserion e Rhaegal."
+  }
+];
 
-  export default cards
+export default cards;
